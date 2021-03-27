557e1572c8ce32bbae80007c3448f49c41be0dd4
Paul Landes <landes@mailc.net>
2021-03-26 22:59:49 -0500

    add model results to readme

--- train/src/mkpkg.py
+++ train/src/mkpkg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from typing import List
+from typing import List, Iterable
 from dataclasses import dataclass
 import logging
 from pathlib import Path
@@ -17,7 +17,9 @@ class MakePackage(object):
     dst_model: str
     package: Path
 
-    def get_metrics(self) -> str:
+    def get_metrics(self) -> List[str]:
+        if hasattr(self, '_mets'):
+            return self._mets
         logger.info(f'parsing validation file for output metrics: {self.metrics}')
         is_mets = False
         mets: List[str]
@@ -30,9 +32,10 @@ class MakePackage(object):
                     is_mets = False
                 elif is_mets:
                     mets.append(line)
-        return '\n'.join(mets)
+        self._mets = mets
+        return mets
 
-    def install(self):
+    def _install_model(self):
         model_path = self.package / 'model'
         metrics_path = model_path / 'model_data.txt'
         logger.info(f'copying metrics to {metrics_path}')
@@ -40,7 +43,8 @@ class MakePackage(object):
             raise ValueError(
                 f'expecting to overrite metrics file: {metrics_path}')
         with open(metrics_path, 'w') as f:
-            f.write(self.get_metrics())
+            f.write('\n'.join(self.get_metrics()))
+            f.write('\n')
         src_model = self.src_model
         if src_model.is_file():
             dst_model = model_path / 'n2c2_model.pkl'
@@ -57,6 +61,37 @@ class MakePackage(object):
             logger.info(f'copying model from {src_model} to {dst_model}')
             shutil.rmtree(dst_model)
             shutil.copytree(src_model, dst_model)
+
+    def get_stats(self) -> Iterable[str]:
+        stats = filter(lambda s: s.startswith('|'), self.get_metrics())
+        stats = map(lambda s: s.replace('+', '|'), stats)
+        return stats
+
+    def _write_readme_stats(self):
+        stats = self.get_stats()
+        readme = self.package.parent / 'README.md'
+        if not readme.is_file():
+            raise ValueError(f'missing readme file: {readme}')
+        logger.info(f'rewriting {readme}')
+        sec = 0
+        new_rm = []
+        with open(readme) as f:
+            for line in map(lambda ln: ln.strip(), f.readlines()):
+                if line.startswith('|'):
+                    if sec == 0:
+                        sec = 1
+                elif sec == 1:
+                    sec = 2
+                    new_rm.extend(stats)
+                if sec == 0 or sec == 2:
+                    new_rm.append(line)
+        with open(readme, 'w') as f:
+            f.write('\n'.join(new_rm))
+            f.write('\n')
+
+    def install(self):
+        self._install_model()
+        self._write_readme_stats()
 
 
 @plac.annotations(
